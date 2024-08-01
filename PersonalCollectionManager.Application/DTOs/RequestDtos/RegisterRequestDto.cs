0bd3dc453fd3544d848f177c745ff13c82c490aa
enayet329 <mdsakayethossen2@gmail.com>
2024-08-01 21:04:10 +0600

    Create Request Dto

--- PersonalCollectionManager.Application/DTOs/RequestDtos/RegisterRequestDto.cs
+++ PersonalCollectionManager.Application/DTOs/RequestDtos/RegisterRequestDto.cs
@@ -4,7 +4,6 @@ namespace PersonalCollectionManager.Application.DTOs.RequestDtos
 {
     public class RegisterRequestDto
     {
-        public Guid Id { get; set; }
         public string Username { get; set; }
         public string Email { get; set; }
         public string PasswordHash { get; set; }
