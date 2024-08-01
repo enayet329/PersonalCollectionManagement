82d215a9a145061176c25d72067bad1a99fe45ad
enayet329 <mdsakayethossen2@gmail.com>
2024-08-01 16:14:49 +0600

    Add Collection Controller

---
+++ PersonalCollectionManager.API/Controllers/CollectionController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PersonalCollectionManager.Application.DTOs.RequestDtos;
+using PersonalCollectionManager.Application.DTOs.ResponseDtos;
+using PersonalCollectionManager.Application.Interfaces.IServices;
+
+
+namespace PersonalCollectionManager.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CollectionController : ControllerBase
+    {
+        private readonly ICollectionService _collectionService;
+        public CollectionController(ICollectionService collectionService)
+        {
+            _collectionService = collectionService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CollectionDTO>>> GetAllColleciton()
+        {
+            var user = await _collectionService.GetAllCollectionsAsync();
+            return Ok(user);
+        }
+
+        [HttpPost("add/collection")]
+        public IActionResult AddCollection(CreateCollectionDto collection)
+        {
+            var result = _collectionService.AddCollectionAsync(collection);
+            return Ok(result);
+        }
+
+        [HttpDelete("delete/collection")]
+        public IActionResult DeleteCollection(Guid id)
+        {
+            var result = _collectionService.DeleteCollectionAsync(id);
+            return Ok(result);
+        }
+
+        [HttpGet("get/collection/id")]
+        public async Task<IActionResult> GetCollectionById(Guid id)
+        {
+            var result = await _collectionService.GetCollectionByIdAsync(id);
+
+            if (result == null)
+            {
+                return NotFound(new { message = "User not found." });
+            }
+
+            return Ok(result);
+        }
+
+        [HttpPut("update/collection")]
+        public IActionResult UpdateCollection(CreateCollectionDto collection)
+        {
+            var result = _collectionService.UpdateCollectionAsync(collection);
+            return Ok(result);
+        }
+    }
+}
