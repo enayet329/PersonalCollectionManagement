4bf0a8a770de970b148db49945b15ec9c26e23dc
enayet329 <mdsakayethossen2@gmail.com>
2024-08-01 22:06:30 +0600

    Add ItemRequestDto

---
+++ PersonalCollectionManager.Application/DTOs/RequestDtos/ItemRequestDto.cs
@@ -0,0 +1,14 @@
+﻿
+using PersonalCollectionManager.Application.DTOs.ResponseDtos;
+
+namespace PersonalCollectionManager.Application.DTOs.RequestDtos
+{
+    public class ItemRequestDto
+    {
+        public string Name { get; set; }
+        public string ImgUrl { get; set; }
+        public string Description { get; set; }
+        public Guid CollectionId { get; set; }
+
+    }
+}
