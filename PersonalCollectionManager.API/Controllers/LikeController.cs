35846d76b58282aa040bbf9f70d025af96093615
enayet329 <mdsakayethossen2@gmail.com>
2024-08-02 02:28:34 +0600

    Successfully Complete LikeController

---
+++ PersonalCollectionManager.API/Controllers/LikeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PersonalCollectionManager.Application.DTOs.RequestDtos;
+using PersonalCollectionManager.Application.Interfaces.IServices;
+
+namespace PersonalCollectionManager.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LikeController : ControllerBase
+    {
+        private readonly ILikeService _like;
+        public LikeController(ILikeService like)
+        {
+            _like = like;
+        }
+
+        [HttpPost("tagle/like")]
+        public async Task<IActionResult> TagleLike(LikeRequestDto request)
+        {
+           var result = await _like.ToggleLike(request);
+            return Ok(result);
+        }
+
+        [HttpPost("get/likes")]
+        public async Task<IActionResult> GetLikes(Guid id)
+        {
+            var likes = await _like.GetAllLikeByItemId(id);
+            return Ok(likes);
+        }
+    }
+}
