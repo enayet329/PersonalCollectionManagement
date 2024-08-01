2eefb3e0db511ceee69e24327223cb281f560861
enayet329 <mdsakayethossen2@gmail.com>
2024-08-01 15:45:59 +0600

    Implement User Repository

---
+++ PersonalCollectionManager.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using PersonalCollectionManager.Application.Interfaces.IRepository;
+using PersonalCollectionManager.Domain.Entities;
+using PersonalCollectionManager.Infrastructure.Data;
+using PersonalCollectionManager.Infrastructure.Repositories;
+using System.Threading.Tasks;
+
+namespace PersonalCollectionManager.Data.Repositories
+{
+    public class UserRepository : Repository<User>, IUserRepository
+    {
+        public UserRepository(AppDbContext context, ILogger<Repository<User>> logger)
+            : base(context, logger) { }
+
+
+        public async Task<User?> GetUserByEmailAsync(string email)
+        {
+            return await _context.Set<User>().FirstOrDefaultAsync(u => u.Email == email);
+        }
+        public async Task<User?> GetUserByUserName(string userName)
+        {
+            return await _context.Set<User>().FirstOrDefaultAsync(u => u.Username == userName);
+        }
+
+        public async Task<User?> GetUserByUserNameAsync(string username)
+        {
+            return await _context.Set<User>().FirstOrDefaultAsync(u => u.Username == username);
+        }
+
+    }
+}
