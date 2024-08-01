1390eb2ec82cc0208b27b562d7a0bcfcafebbeb3
enayet329 <mdsakayethossen2@gmail.com>
2024-08-01 15:41:01 +0600

    Add Auto Mapper Profile

---
+++ PersonalCollectionManager.Shared/Helpers/AutoMapperProfile.cs
@@ -0,0 +1,34 @@
+﻿
+using AutoMapper;
+using PersonalCollectionManager.Application.DTOs.RequestDtos;
+using PersonalCollectionManager.Application.DTOs.ResponseDtos;
+using PersonalCollectionManager.Domain.Entities;
+
+namespace PersonalCollectionManager.Shared.Helpers
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            // ResponseDTOs to Domain Entities
+            CreateMap<Collection, CollectionDTO>().ReverseMap();
+            CreateMap<Comment, CommentDTO>().ReverseMap();
+            CreateMap<Item, ItemDTO>().ReverseMap();
+            CreateMap<Like, LikeDTO>().ReverseMap();
+            CreateMap<Tag, TagDTO>().ReverseMap();
+            CreateMap<User, UserDTO>().ReverseMap();
+
+            // RequestDTOs to Domain Entities
+            CreateMap<RegisterRequestDto, User>()
+                .ForMember(dest => dest.Collections, opt => opt.Ignore())
+                .ForMember(dest => dest.Comments, opt => opt.Ignore())
+                .ForMember(dest => dest.Likes, opt => opt.Ignore());
+            CreateMap<User, RegisterRequestDto>();
+
+            CreateMap<CreateCollectionDto, Collection>()
+                .ForMember(dest => dest.Items, opt => opt.Ignore());
+            CreateMap<Collection, CreateCollectionDto>();
+
+        }
+    }
+}
