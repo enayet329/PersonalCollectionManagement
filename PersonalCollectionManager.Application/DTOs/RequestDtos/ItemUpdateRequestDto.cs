d982c14402da696682f2996fbdd7951f28720ab9
enayet329 <mdsakayethossen2@gmail.com>
2024-08-02 15:33:01 +0600

    Add new Dto for UpdateItem

---
+++ PersonalCollectionManager.Application/DTOs/RequestDtos/ItemUpdateRequestDto.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace PersonalCollectionManager.Application.DTOs.RequestDtos
+{
+    public class ItemUpdateRequestDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string ImgUrl { get; set; }
+        public string Description { get; set; }
+        public Guid CollectionId { get; set; }
+    }
+}
