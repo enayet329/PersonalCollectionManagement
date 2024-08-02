fcf0c65b0720e6bab02eb050f90a3db8a560c097
enayet329 <mdsakayethossen2@gmail.com>
2024-08-02 15:12:50 +0600

    Add interface To find Top 5 Largest Collection

--- PersonalCollectionManager.Application/Interfaces/IRepository/ICollectionRepository.cs
+++ PersonalCollectionManager.Application/Interfaces/IRepository/ICollectionRepository.cs
@@ -8,5 +8,8 @@ namespace PersonalCollectionManager.Application.Interfaces.IRepository
     {
         Task<IEnumerable<Collection>> GetAllCollectionAsync();
         Task<Collection> GetCollectionByIdAsync(Guid id);
+        Task<IEnumerable<Collection>> GetCollectionsByUserIdAsync(Guid userId);
+        Task<Collection> GetCollectionByUserIdAsync(Guid userId);
+        Task<IEnumerable<Collection>> GetLargestCollectionsAsync(int count);
     }
 }
