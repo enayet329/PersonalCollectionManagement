ff001a9e0e9cabf21351e8649efa607955cda448
enayet329 <mdsakayethossen2@gmail.com>
2024-08-02 12:40:38 +0600

    Add new Interface based on  project requirement

--- PersonalCollectionManager.Application/Interfaces/IServices/ICollectionService.cs
+++ PersonalCollectionManager.Application/Interfaces/IServices/ICollectionService.cs
@@ -8,6 +8,13 @@ namespace PersonalCollectionManager.Application.Interfaces.IServices
     public interface ICollectionService 
     {
         Task<IEnumerable<CollectionDTO>> GetAllCollectionsAsync();
+
+        // TODO: Implement
+        Task<IEnumerable<CollectionDTO>> GetAllCollectionsByUserIdAsync(Guid id);
+        Task<IEnumerable<CollectionDTO>> GetLargestCollecitonAsync();
+        Task<IEnumerable<CollectionDTO>> GetLatestCollectionAsync();
+        Task<CollectionDTO> GetCollectionByUserIdAsync(Guid id);
+
         Task<CollectionDTO> GetCollectionByIdAsync(Guid id);
         Task<OperationResult> AddCollectionAsync(CollectionRequestDto collection);
         Task<OperationResult> UpdateCollectionAsync(CollectionRequestDto collection);
