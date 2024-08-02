6a04ebf5eb9b74f25a12bb5db304ad4537614076
enayet329 <mdsakayethossen2@gmail.com>
2024-08-02 15:15:20 +0600

    implement Collection Repository interface

--- PersonalCollectionManager.Infrastructure/Repositories/CollectionRepository.cs
+++ PersonalCollectionManager.Infrastructure/Repositories/CollectionRepository.cs
@@ -1,26 +1,46 @@
-﻿
-
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
-using PersonalCollectionManager.Application.DTOs.ResponseDtos;
 using PersonalCollectionManager.Application.Interfaces.IRepository;
 using PersonalCollectionManager.Domain.Entities;
 using PersonalCollectionManager.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace PersonalCollectionManager.Infrastructure.Repositories
 {
-    public class CollectionRepository : Repository<Collection>, ICollectionRepository    {
+    public class CollectionRepository : Repository<Collection>, ICollectionRepository
+    {
         public CollectionRepository(AppDbContext context, ILogger<Repository<Collection>> logger)
             : base(context, logger) { }
 
         public async Task<IEnumerable<Collection>> GetAllCollectionAsync()
         {
-                return await _context.Set<Collection>().ToListAsync();
+            return await _context.Set<Collection>().ToListAsync();
         }
 
-        public async Task<Collection> GetCollectionByIdAsync(Guid id)
+        public async Task<Collection?> GetCollectionByIdAsync(Guid id)
         {
             return await _context.Set<Collection>().FindAsync(id);
+        }
+
+        public async Task<Collection?> GetCollectionByUserIdAsync(Guid userId)
+        {
+            return await _context.Set<Collection>().FirstOrDefaultAsync(c => c.UserId == userId);
+        }
+
+        public async Task<IEnumerable<Collection>> GetCollectionsByUserIdAsync(Guid userId)
+        {
+            return await _context.Set<Collection>().Where(c => c.UserId == userId).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Collection>> GetLargestCollectionsAsync(int count)
+        {
+            return await _context.Set<Collection>()
+                .OrderByDescending(c => c.Items.Count)
+                .Take(count)
+                .ToListAsync();
         }
     }
 }
