b9fe5a43d3c3fab34f93352022e96ba267d1bbb2
enayet329 <mdsakayethossen2@gmail.com>
2024-08-03 23:24:35 +0600

    Implement IAuthRepository - Implement a method for get user by userId

---
+++ PersonalCollectionManager.Infrastructure/Repositories/AuthRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using PersonalCollectionManager.Application.Interfaces.IRepository;
+using PersonalCollectionManager.Domain.Entities;
+using PersonalCollectionManager.Infrastructure.Data;
+
+
+namespace PersonalCollectionManager.Infrastructure.Repositories
+{
+    public class AuthRepository : Repository<RefreshToken>, IAuthRepository
+    {
+        public AuthRepository(AppDbContext context,ILogger<AuthRepository> logger) : base(context, logger) { }
+
+        public async Task<RefreshToken?> GetRefreshToken(Guid userId)
+        {
+            return await _context.Set<RefreshToken>().FirstOrDefaultAsync(x => x.UserId == userId);
+        }
+
+    }
+}
