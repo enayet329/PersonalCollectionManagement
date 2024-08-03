1ccacac882e277bcede00b8a10bc955d724375da
enayet329 <mdsakayethossen2@gmail.com>
2024-08-03 23:32:17 +0600

    Add two new method - change theme and language

--- PersonalCollectionManager.Application/Interfaces/IServices/IAccountService.cs
+++ PersonalCollectionManager.Application/Interfaces/IServices/IAccountService.cs
@@ -9,10 +9,13 @@ namespace PersonalCollectionManager.Application.Interfaces.IServices
     {
         Task<OperationResult> Register(RegisterRequestDto userDto);
         Task<OperationResult> Login(LoginRequestDTO loginRequestDto);
+        Task<OperationResult> GetRefreshToken(RefreshTokenRequestDto refreshToken);
         Task<OperationResult> UpdateUser(UserDto userDto);
         Task<UserDto> GetUserByIdAsync(Guid id);
         Task<UserDto> GetUserByUseremailAsync(string email);
         Task<bool> IsUsernameAvailableAsync(string username);
         Task<bool> IsEmailAvailableAsync(string email);
+        Task<bool> ChangeThemeAsync(Guid userId,bool isDarkMode);
+        Task<bool> ChangeLanguageAsync(Guid userId,String language);
     }
 }
