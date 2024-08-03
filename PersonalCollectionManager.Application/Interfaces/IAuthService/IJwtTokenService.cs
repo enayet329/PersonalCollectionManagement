3f2a5ee654bdc604827c3d8d52c73ef01f88a290
enayet329 <mdsakayethossen2@gmail.com>
2024-08-03 10:24:11 +0600

    Add new Interface for Jwt creating and validation service

---
+++ PersonalCollectionManager.Application/Interfaces/IAuthService/IJwtTokenService.cs
@@ -0,0 +1,9 @@
+﻿
+namespace PersonalCollectionManager.Application.Interfaces.IAuthService
+{
+    public interface IJwtTokenService
+    {
+        string GenerateToken(string user);
+        bool ValidateToken(string token);
+    }
+}
