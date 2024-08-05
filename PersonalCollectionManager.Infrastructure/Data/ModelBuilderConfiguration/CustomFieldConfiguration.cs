34b8201e7000e23469e2569c4c137a77717fbe83
enayet329 <mdsakayethossen2@gmail.com>
2024-08-05 20:08:04 +0600

    Update DbContext and OnModelbuilder configuration based on customFiled and customFieldValue entity

---
+++ PersonalCollectionManager.Infrastructure/Data/ModelBuilderConfiguration/CustomFieldConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PersonalCollectionManager.Domain.Entities;
+
+namespace PersonalCollectionManager.Infrastructure.Data.ModelBuilderConfiguration
+{
+    public class CustomFieldConfiguration : IEntityTypeConfiguration<CustomField>
+    {
+        public void Configure(EntityTypeBuilder<CustomField> builder)
+        {
+            builder.HasKey(cf => cf.Id);
+
+            builder.HasIndex(cf => cf.Id).IsUnique();
+
+            builder.Property(cf => cf.Name)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.Property(cf => cf.FieldType)
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.HasOne(cf => cf.Collection)
+                .WithMany(c => c.CustomFields)
+                .HasForeignKey(cf => cf.CollectionId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasMany(cf => cf.CustomFieldValues)
+                .WithOne(cfv => cfv.CustomField)
+                .HasForeignKey(cfv => cfv.CustomFieldId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
