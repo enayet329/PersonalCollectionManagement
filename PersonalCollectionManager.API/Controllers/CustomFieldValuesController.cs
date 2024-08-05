e7dc4ef9e6c04ec871f48b3eab86a1c87ffae531
enayet329 <mdsakayethossen2@gmail.com>
2024-08-05 20:15:57 +0600

    Add Controller for CustomField and CustomFieldValue - Implement Api EndPoint

---
+++ PersonalCollectionManager.API/Controllers/CustomFieldValuesController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PersonalCollectionManager.Application.DTOs.RequestDtos;
+using PersonalCollectionManager.Application.Interfaces.IServices;
+using PersonalCollectionManager.Domain.Entities;
+
+namespace PersonalCollectionManager.API.Controllers
+{
+    [Route("api/v1/custom-field-valus")]
+    [ApiController]
+    public class CustomFieldValuesController : ControllerBase
+    {
+        private readonly ICustomFieldService _customFieldService;
+        public CustomFieldValuesController(ICustomFieldService customFieldService)
+        {
+            _customFieldService = customFieldService;
+        }
+
+
+        [HttpGet("{itemId}")]
+        public async Task<IActionResult> GetCustomFieldValues(Guid itemId)
+        {
+            var customFieldValues = await _customFieldService.GetCustomFieldValuesByItemIdAsync(itemId);
+            return Ok(customFieldValues);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddCustomFieldValue([FromBody] IEnumerable<CustomFieldValueCreateDto> customFieldValue)
+        {
+            await _customFieldService.AddCustomFieldValueAsync(customFieldValue);
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateCustomFieldValue([FromBody] IEnumerable<CustomFieldValueUpdateDto> customFieldValue)
+        {
+            await _customFieldService.UpdateCustomFieldValueAsync(customFieldValue);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCustomFieldValue(Guid id)
+        {
+            await _customFieldService.DeleteCustomFieldValueAsync(id);
+            return Ok();
+        }
+    }
+}
---
+++ PersonalCollectionManager.API/Controllers/CustomFieldsController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PersonalCollectionManager.Application.DTOs.RequestDtos;
+using PersonalCollectionManager.Application.Interfaces.IServices;
+using PersonalCollectionManager.Domain.Entities;
+using PersonalCollectionManager.Infrastructure.Services;
+
+namespace PersonalCollectionManager.API.Controllers
+{
+    [Route("api/custom-field")]
+    [ApiController]
+    public class CustomFieldsController : ControllerBase
+    {
+        private readonly ICustomFieldService _customFieldService;
+        public CustomFieldsController(ICustomFieldService customFieldService)
+        {
+            _customFieldService = customFieldService;
+        }
+
+        [HttpGet("{collectionId}")]
+        public async Task<IActionResult> GetCustomFields(Guid collectionId)
+        {
+            var customFields = await _customFieldService.GetCustomFieldsByCollectionIdAsync(collectionId);
+            return Ok(customFields);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddCustomField([FromBody] IEnumerable<CustomFieldCreateDto> customField)
+        {
+            await _customFieldService.AddCustomFieldAsync(customField);
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateCustomField([FromBody] IEnumerable<CustomFieldUpdateDto> customField)
+        {
+            await _customFieldService.UpdateCustomFieldAsync(customField);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCustomField(Guid id)
+        {
+            await _customFieldService.DeleteCustomFieldAsync(id);
+            return Ok();
+        }
+    }
+}
