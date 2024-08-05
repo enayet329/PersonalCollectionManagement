c4ab26fb3babc4bbfb36e1cb7df4988ec107aa84
enayet329 <mdsakayethossen2@gmail.com>
2024-08-05 20:06:46 +0600

    Add customField Dto

---
+++ PersonalCollectionManager.Application/DTOs/RequestDtos/CustomFieldCreateDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalCollectionManager.Application.DTOs.RequestDtos
+{
+    public class CustomFieldCreateDto
+    {
+        public string Name { get; set; }
+        public string FieldType { get; set; }
+        public Guid CollectionId { get; set; }
+    }
+
+}
---
+++ PersonalCollectionManager.Application/DTOs/RequestDtos/CustomFieldUpdateDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalCollectionManager.Application.DTOs.RequestDtos
+{
+    public class CustomFieldUpdateDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string FieldType { get; set; }
+        public Guid CollectionId { get; set; }
+    }
+}
