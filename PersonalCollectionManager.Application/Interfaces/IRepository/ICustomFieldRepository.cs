f2e23a7d38563693ddedfa97402aae4c2d1e3bd2
enayet329 <mdsakayethossen2@gmail.com>
2024-08-05 20:09:17 +0600

    Add interface for customField and customFieldValue - implement crud operation method

---
+++ PersonalCollectionManager.Application/Interfaces/IRepository/ICustomFieldRepository.cs
@@ -0,0 +1,11 @@
+﻿
+using PersonalCollectionManager.Domain.Entities;
+
+namespace PersonalCollectionManager.Application.Interfaces.IRepository
+{
+    public interface ICustomFieldRepository : IRepository<CustomField>
+    {
+        Task AddRangeAsync(IEnumerable<CustomField> customFields);
+        Task<IEnumerable<CustomField>> GetByCollectionIdAsync(Guid collectionId);
+    }
+}
