246a7b5e9b6aba0e8ead30cd835c3732f79e978d
enayet329 <mdsakayethossen2@gmail.com>
2024-08-06 19:29:03 +0600

    Update Dto

---
+++ PersonalCollectionManager.Application/DTOs/ResponseDtos/ItemAlgoliaDto.cs
@@ -0,0 +1,13 @@
+﻿namespace PersonalCollectionManager.Application.DTOs.ResponseDtos
+{
+    public class ItemAlgoliaDto
+    {
+        public string ObjectID { get; set; }
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImgUrl { get; set; }
+        public DateTime DateAdded { get; set; }
+        public Guid CollectionId { get; set; }
+    }
+}
