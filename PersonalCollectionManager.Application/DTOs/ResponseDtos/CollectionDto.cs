3ee6269a78e3a1a578950dbcea95ab5bbf289be0
enayet329 <mdsakayethossen2@gmail.com>
2024-08-07 00:14:31 +0600

    Add Dto field for get UserName and  Number of item

--- PersonalCollectionManager.Application/DTOs/ResponseDtos/CollectionDto.cs
+++ PersonalCollectionManager.Application/DTOs/ResponseDtos/CollectionDto.cs
@@ -1,12 +1,16 @@
-﻿namespace PersonalCollectionManager.Application.DTOs.ResponseDtos
+﻿using PersonalCollectionManager.Domain.Entities;
+
+namespace PersonalCollectionManager.Application.DTOs.ResponseDtos
 {
     public class CollectionDto
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
+        public string? Description { get; set; }
         public string Topic { get; set; }
         public string ImageUrl { get; set; }
         public Guid UserId { get; set; }
+        public string UserName { get; set; }
+        public int ItemCount { get; set; }
     }
 }
