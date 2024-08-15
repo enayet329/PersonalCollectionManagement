7b2510ebbbc0a59cb1fb472ad826748f75426bb0
enayet329 <mdsakayethossen2@gmail.com>
2024-08-16 01:59:14 +0600

    Implement categoryService interface

---
+++ PersonalCollectionManager.Infrastructure/Services/CategoryService.cs
@@ -0,0 +1,51 @@
+﻿using PersonalCollectionManager.Application.Interfaces.IRepository;
+using PersonalCollectionManager.Application.Interfaces.IServices;
+using PersonalCollectionManager.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalCollectionManager.Infrastructure.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        public CategoryService(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public async Task<bool> AddCategory(Category category)
+        {
+            try
+            {
+                await _categoryRepository.AddAsync(category);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public async Task<IEnumerable<Category>> GetCategories()
+        {
+            return await _categoryRepository.GetAll();
+        }
+
+        public Task<bool> RemoveCategory(Category category)
+        {
+            try
+            {
+                _categoryRepository.Remove(category);
+                return Task.FromResult(true);
+            }
+            catch (Exception)
+            {
+                return Task.FromResult(false);
+            }
+        }
+    }
+}
