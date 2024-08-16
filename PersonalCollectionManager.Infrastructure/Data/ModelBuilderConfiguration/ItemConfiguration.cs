570079e3b92a0dd692cf4e6c5981258b74ccc3a1
enayet329 <mdsakayethossen2@gmail.com>
2024-08-16 19:28:53 +0600

    feat(customField) remove cascade deletion configuration

--- PersonalCollectionManager.Infrastructure/Data/ModelBuilderConfiguration/ItemConfiguration.cs
+++ PersonalCollectionManager.Infrastructure/Data/ModelBuilderConfiguration/ItemConfiguration.cs
@@ -1,40 +1,37 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Builders;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
 using PersonalCollectionManager.Domain.Entities;
 
-namespace PersonalCollectionManager.Infrastructure.Data.ModelBuilderConfiguration
+public class ItemConfiguration : IEntityTypeConfiguration<Item>
 {
-    public class ItemConfiguration : IEntityTypeConfiguration<Item>
+    public void Configure(EntityTypeBuilder<Item> builder)
     {
-        public void Configure(EntityTypeBuilder<Item> builder)
-        {
-            builder.HasKey(i => i.Id);
+        builder.HasKey(i => i.Id);
 
-            builder.HasIndex(i => i.Id).IsUnique();
+        builder.HasIndex(i => i.Id).IsUnique();
 
-            builder.Property(i => i.Name)
-                .HasMaxLength(100)
-                .IsRequired();
+        builder.Property(i => i.Name)
+            .HasMaxLength(100)
+            .IsRequired();
 
-            builder.HasOne(i => i.Collection)
-                .WithMany(c => c.Items)
-                .HasForeignKey(i => i.CollectionId)
-                .OnDelete(DeleteBehavior.Cascade);
+        builder.HasOne(i => i.Collection)
+            .WithMany(c => c.Items)
+            .HasForeignKey(i => i.CollectionId)
+            .OnDelete(DeleteBehavior.Cascade);
 
-            builder.HasMany(i => i.Comments)
-                .WithOne(c => c.Item)
-                .HasForeignKey(c => c.ItemId)
-                .OnDelete(DeleteBehavior.Cascade);
+        builder.HasMany(i => i.Comments)
+            .WithOne(c => c.Item)
+            .HasForeignKey(c => c.ItemId)
+            .OnDelete(DeleteBehavior.Cascade);
 
-            builder.HasMany(i => i.Likes)
-                .WithOne(l => l.Item)
-                .HasForeignKey(l => l.ItemId)
-                .OnDelete(DeleteBehavior.Cascade);
+        builder.HasMany(i => i.Likes)
+            .WithOne(l => l.Item)
+            .HasForeignKey(l => l.ItemId)
+            .OnDelete(DeleteBehavior.Cascade);
 
-            builder.HasMany(i => i.CustomFieldValues)
-                .WithOne(cfv => cfv.Item)
-                .HasForeignKey(cfv => cfv.ItemId)
-                .OnDelete(DeleteBehavior.Cascade);
-        }
+        builder.HasMany(i => i.CustomFieldValues)
+            .WithOne(cfv => cfv.Item)
+            .HasForeignKey(cfv => cfv.ItemId)
+            .OnDelete(DeleteBehavior.Cascade);
     }
 }
