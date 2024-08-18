08e8bb94d5876b6d3d73f8a55233d156a8432f72
enayet329 <mdsakayethossen2@gmail.com>
2024-08-18 15:54:30 +0600

    feat(iTagRepository): add a new method for get all uniqe tag

--- PersonalCollectionManager.Application/Interfaces/IRepository/ITagRepository.cs
+++ PersonalCollectionManager.Application/Interfaces/IRepository/ITagRepository.cs
@@ -5,6 +5,7 @@ namespace PersonalCollectionManager.Application.Interfaces.IRepository
 {
     public interface ITagRepository : IRepository<Tag>
     {
+        Task<IEnumerable<Tag>> GatAllTagsAsync();
         Task<IEnumerable<Tag>> GetByItemId(Guid id);
         Task<Tag> GetTagWithItemTagAsync(Guid tagId);
         Task<IEnumerable<Tag>> GetTopTagsAsync();
