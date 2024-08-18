fbf9ede7e99fad77d647d6e83e9bda94bd262095
enayet329 <mdsakayethossen2@gmail.com>
2024-08-18 15:55:06 +0600

    feat(collecitonUpdateDto): add new entity for update colleciton

---
+++ PersonalCollectionManager.Application/DTOs/RequestDtos/CollectionUpdateDto.cs
@@ -0,0 +1,13 @@
+﻿
+namespace PersonalCollectionManager.Application.DTOs.RequestDtos
+{
+    public class CollectionUpdateDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string topic { get; set; }
+        public string imageUrl { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
