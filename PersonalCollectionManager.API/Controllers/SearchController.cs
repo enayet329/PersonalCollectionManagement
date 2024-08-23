fe32a09d609b1cba6dd4e955681719b57d55eb5c
enayet329 <mdsakayethossen2@gmail.com>
2024-08-23 12:03:58 +0600

    feat(searchController) add a controller for full text search

---
+++ PersonalCollectionManager.API/Controllers/SearchController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PersonalCollectionManager.Application.DTOs.ResponseDtos;
+using PersonalCollectionManager.Domain.Entities;
+
+namespace PersonalCollectionManager.API.Controllers
+{
+    [Route("api/v1/search")]
+    [ApiController]
+    public class SearchController : ControllerBase
+    {
+        private readonly AlgoliaItemService _algoliaItemService;
+        public SearchController(AlgoliaItemService algoliaItemService)
+        {
+            _algoliaItemService = algoliaItemService;
+        }
+
+        [HttpGet("full-text-search")]
+        public async Task<IActionResult> Search(string query)
+        {
+            var items = await _algoliaItemService.SearchAsync<AlgoliaItemDto>(query);
+
+            return Ok(items.Hits);
+        }
+    }
+}
